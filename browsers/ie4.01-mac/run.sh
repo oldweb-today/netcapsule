287162b37a03213f76476ffe539b2add6a8961f7
Ilya Kreymer <ikreymer@gmail.com>
2015-10-28 20:04:10 -0700

    basilisk browsers: use diff rom add ie4.01 on mac! remove unused files from netscape 3

---
+++ browsers/ie4.01-mac/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo chown browser:browser -R /app
+
+mkdir /app/share
+
+PYWB_IP=$(grep netcapsule_pywb_1 /etc/hosts | cut -f 1 | head -n 1)
+
+# Proxy
+echo -n -e "$PYWB_IP:8080\r" > /app/share/proxy_prefs
+
+# URL
+echo -n -e "$URL\r" >> /app/share/proxy_prefs
+
+BasiliskII-jit --config /app/basilisk_ii_prefs --extfs /app/share/ --display $DISPLAY
+
--- browsers/netscape-mac-3.04/run.sh
+++ browsers/netscape-mac-3.04/run.sh
@@ -6,11 +6,5 @@ mkdir /app/share
 
 PYWB_IP=$(grep netcapsule_pywb_1 /etc/hosts | cut -f 1 | head -n 1)
 
-sed -b -i s/netcapsule_pywb_1/$PYWB_IP/g /app/NetscapePreferences
-
-awk -v RS="\r" -v URL="$URL" '{gsub("HOME_PAGE_URL", URL, $0); print}' /app/NetscapePreferences > /tmp/prefs.tmp
-
-mv /tmp/prefs.tmp "/app/share/Netscape Preferences"
-
-BasiliskII-jit --config /app/basilisk_ii_prefs --extfs /app/share/ --display $DISPLAY
+BasiliskII-jit --config /app/basilisk_ii_prefs --display $DISPLAY
 
