8bfb99657cf0140bb884fe4abc2d3832b14a28f4
Ilya Kreymer <ikreymer@gmail.com>
2015-10-29 22:16:22 -0700

    add netscape4.8 win

---
+++ browsers/netscape4.8-win/prefs.js
@@ -0,0 +1,11 @@
+user_pref("network.proxy.http", "netcapsule_pywb_1");
+user_pref("network.proxy.http_port", 8080);
+user_pref("network.proxy.ssl", "netcapsule_pywb_1");
+user_pref("network.proxy.ssl_port", 8080);
+user_pref("network.proxy.type", 1);
+
+user_pref("browser.related.enabled", false);
+user_pref("browser.startup.homepage_override", false);
+user_pref("browser.wfe.show_value", 3);
+user_pref("taskbar.floating", false);
+
---
+++ browsers/netscape4.8-win/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export WINEPREFIX="/home/browser/ns48"
+
+PYWB_IP=$(grep netcapsule_pywb_1 /etc/hosts | cut -f 1 | head -n 1)
+
+sed s/netcapsule_pywb_1/$PYWB_IP/g /home/browser/prefs.js > /home/browser/ns48/drive_c/Program\ Files/Netscape/Users/default/prefs.js
+
+
+wine 'C:/Program Files/Netscape/Communicator/Program/netscape.exe' $URL
