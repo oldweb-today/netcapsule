302ca7a481f3e3c5af7ef0f19af8be78e50e6bab
Ilya Kreymer <ikreymer@gmail.com>
2015-11-22 22:37:59 -0800

    ui work: add index page (from @despens), browser menu working move shared browser menu controls to shared.js from main.js

--- app/static/main.js
+++ app/static/main.js
@@ -335,107 +335,16 @@ $(function() {
             UIresize();
         }, 500);
     };
-    
-    // INIT
-    init_container();
-});
 
-
-
-// Dropdowns
-$(function() {
-
-
-    // Shared
-    $(".menu-selector-close").click(function(e) {
-        hide_menu();
-    });
-
-    $(document).click(function(e){
-        hide_menu();
-    });
-
-    $(".selector-menu").click(function(e){
-        e.stopPropagation();
-    });
-
-    $(".drop-skip").click(function(e){
-        e.stopPropagation();
-    });
-
-    function hide_menu()
-    {
-        $(".selector-menu").hide();
-        $(".dropdown").removeClass("dropdown-shown");
-    }
-
-
-    // Browser
-    $("#browser-dropdown").click(function(e) {
-        if (!$(".selector-menu").is(":visible")) {
-            show_browser_menu();
-        } else {
-            hide_menu();
-        }
-        e.stopPropagation();
-    });
-
+    // Browser navigate
     $("#browser-selector td:not(:empty)").click(function(e) {
-        $("#browser-selector td").removeClass("selected");
-        $(this).addClass("selected");
-
-        var tr = $(this).parent();
-        var browserTH;
-        do {
-            browserTH = tr.find("th");
-            tr = tr.prev();
-        } while (tr && !browserTH.length);
-
-        var platform = $("#browser-selector thead").find("th").eq($(this).index());
-
-        $("#browser-text").text(browserTH.text() + " on " + platform.text());
-        $("#browser-icon").attr("src", $(this).find("img").attr("src"));
-        $("#browser-label").text($(this).find("label").text());
-
-        hide_menu();
-
         var path = $(this).attr("data-path");
         var full_url = window.location.origin + "/" + path + "/" + curr_ts + "/" + url;
         window.location.href = full_url;
     });
-
-    function show_browser_menu()
-    {
-        $("#browser-selector").show();
-        $("#browser-dropdown").addClass("dropdown-shown");
-
-        var pos = $("#browser-dropdown").offset();
-        pos.top += $("#browser-dropdown").outerHeight();
-        $("#browser-selector").offset(pos);
-    }
-
-    $("#browser-selector td[data-path='" + coll + "']").addClass("selected");
-
-
-    // Datetime
-    $("#datetime-dropdown").click(function(e) {
-        if (!$(".selector-menu").is(":visible")) {
-            show_datetime_menu();
-        } else {
-            hide_menu();
-        }
-        e.stopPropagation();
-    });
-
-    function show_datetime_menu()
-    {
-        $("#datetime-selector").show();
-        $("#datetime-dropdown").addClass("dropdown-shown");
-
-        var pos = $("#datetime-dropdown").offset();
-        pos.top += $("#datetime-dropdown").outerHeight();
-        $("#datetime-selector").offset(pos);
-    }
+    
+    // INIT
+    init_container();
 });
 
 
