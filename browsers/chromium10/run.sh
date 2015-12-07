afda74182e9a6d4aba47559b2734114abc5298fc
Charles Julian Knight <charles@rabidaudio.com>
2015-12-06 22:41:04 -0500

    changed version to 10.0.648 (offical release)

--- browsers/chromium10/run.sh
+++ browsers/chromium10/run.sh
@@ -7,17 +7,17 @@ http_proxy="http://netcapsule_pywb_1:8080" wget "http://pywb.proxy/pywb-ca.pem"
 
 mkdir -p $HOME/.pki/nssdb
 certutil -d $HOME/.pki/nssdb -N
-certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "pywb" -i /home/browser/pywb-ca.pem 
+certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "pywb" -i ~/pywb-ca.pem 
 
-sudo chown -R browser:browser /home/browser/chrome-linux
+sudo chown -R browser:browser ~/chrome-linux
 
 mkdir ~/.config/
 mkdir ~/.config/chromium
 touch ~/.config/chromium/First\ Run
 
 if [ -n "$NO_PROXY" ]; then
-    run_browser /home/browser/chrome-linux/chrome "$URL"
+    run_browser ~/chrome-linux/chrome "$URL"
 else
-    run_browser /home/browser/chrome-linux/chrome --proxy-server="netcapsule_pywb_1:8080"  "$URL"
+    run_browser ~/chrome-linux/chrome --proxy-server="netcapsule_pywb_1:8080"  "$URL"
 fi
 
