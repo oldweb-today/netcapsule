e992a50df55debd1503c3a8150f0301d80b35ada
Ilya Kreymer <ikreymer@gmail.com>
2016-10-10 22:02:15 -0700

    firefox: disable most background requests, support background window opening (for load tabs) proxy settings: rename pywb.ca -> proxy.ca for clarity

--- browsers/firefox/ffprofile/user.js
+++ browsers/firefox/ffprofile/user.js
@@ -1,16 +1,54 @@
+user_pref("app.update.enabled", false);
+
+user_pref("browser.aboutHomeSnippets.updateUrl", "");
+user_pref("browser.readinglist.enabled", false);
+user_pref("browser.readinglist.sidebarEverOpened", false);
+user_pref("browser.safebrowsing.blockedURIs.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.url", "");
+user_pref("browser.safebrowsing.enabled", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.selfsupport.url", "");
+user_pref("browser.search.geoip.url", "");
+user_pref("browser.search.geoSpecificDefaults", false);
+user_pref("browser.search.geoSpecificDefaults.url", "");
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.newtabpage.directory.ping", "");
+user_pref("browser.newtabpage.directory.source", "");
+
+user_pref("datareporting.healthreport.service.enabled", false);
+user_pref("datareporting.healthreport.service.firstRun", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+
+user_pref("dom.disable_open_during_load", false);
+
+user_pref("extensions.blocklist.enabled", false);
+user_pref("extensions.getAddons.cache.enabled", false);
+
+user_pref("geo.enabled", false);
+
+user_pref("media.gmp-provider.enabled", false);
+user_pref("media.gmp-manager.url", "");
+
 user_pref("network.proxy.http", "proxy");
 user_pref("network.proxy.http_port", 8080);
+user_pref("network.proxy.share_proxy_settings", true);
 user_pref("network.proxy.ssl", "proxy");
 user_pref("network.proxy.ssl_port", 8080);
 user_pref("network.proxy.type", 1);
-user_pref("network.proxy.share_proxy_settings", true);
 
-user_pref("datareporting.healthreport.service.firstRun", false);
-user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("privacy.trackingprotection.enabled", false);
+user_pref("privacy.trackingprotection.introURL", "");
+user_pref("privacy.trackingprotection.pbmode.enabled", false);
 
-user_pref("browser.selfsupport.url", "");
-user_pref("geo.enabled", false);
-
-user_pref("browser.readinglist.enabled", false);
-user_pref("browser.readinglist.sidebarEverOpened", false);
 user_pref("reader.parse-on-load.enabled", false);
+
+user_pref("security.OCSP.enabled", 0);
+
+user_pref("services.sync.prefs.sync.safebrowsing.enabled", false);
+user_pref("services.sync.prefs.sync.safebrowsing.malware.enabled", false);
+
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.rejected", true);
--- browsers/firefox/run.sh
+++ browsers/firefox/run.sh
@@ -9,10 +9,10 @@ cd /home/browser/ffprofile
 sudo chown browser:browser /home/browser/ffprofile/*
 chmod 644 /home/browser/ffprofile/*
 
-#curl -x "netcapsule_pywb_1:8080" "http://pywb.proxy/pywb-ca.pem" > /tmp/pywb-ca.pem
-curl -x "proxy:8080"  "http://mitm.it/cert/pem" > /tmp/pywb-ca.pem
+#curl -x "netcapsule_pywb_1:8080" "http://pywb.proxy/proxy-ca.pem" > /tmp/proxy-ca.pem
+curl -x "proxy:8080"  "http://mitm.it/cert/pem" > /tmp/proxy-ca.pem
 
-certutil -A -n "PYWB" -t "TCu,Cuw,Tuw" -i /tmp/pywb-ca.pem -d /home/browser/ffprofile
+certutil -A -n "PROXY" -t "TCu,Cuw,Tuw" -i /tmp/proxy-ca.pem -d /home/browser/ffprofile
 
 #/opt/firefox/firefox --profile /home/browser/ffprofile -setDefaultBrowser --new-window "$URL" -width $SCREEN_WIDTH -height $SCREEN_HEIGHT
 run_browser /opt/firefox/firefox --profile /home/browser/ffprofile -setDefaultBrowser --new-window "$URL"
